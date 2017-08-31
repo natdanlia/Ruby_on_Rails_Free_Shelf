7d567b7cec877414f7dd61f3f0fb29c23ded55d9
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-08-31 11:21:27 -0400

    fourth commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'sessions#new'
   resource :session, only: [:new, :create, :destroy]
+  resource :users, only: [:new, :create]
 end
